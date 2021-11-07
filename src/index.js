7e78e55e4498bebe0139d843c6aa5613fca18c7b
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-11-07 14:26:00 +0300

    17.142

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
+
+  state = {
+    starWarsChars: [
+      {name: 'Дарт Вэйден', side: 'dark'},
+      {name: 'Люк Скайворкер', side: 'light'},
+      {name: 'Палпатин', side: 'dark'},
+      {name: 'Обиван Кеноби', side: 'light'}
+    ]
+  }
+
   render() {
     return (
-      <div>
+      <ul>
+        {this.state.starWarsChars.map((char, index) => {
+          return (
+            <li key={char.name + index}>
+              <strong>{char.name}</strong> - {char.side}
+            </li>
+          )
+        })}
 
-      </div>
+      </ul>
     )
   }
 }
