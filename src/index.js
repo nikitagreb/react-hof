1b4d83f8f4533d08cdc8dd0d404a1bd917cec449
Nikita Grebennikov <nikitagreb0204@mail.ru>
2021-11-07 14:34:55 +0300

    17.143

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-class App extends React.Component {
+const starWarsChars = [
+  {name: 'Дарт Вэйден', side: 'dark'},
+  {name: 'Люк Скайворкер', side: 'light'},
+  {name: 'Палпатин', side: 'dark'},
+  {name: 'Обиван Кеноби', side: 'light'}
+]
 
-  state = {
-    starWarsChars: [
-      {name: 'Дарт Вэйден', side: 'dark'},
-      {name: 'Люк Скайворкер', side: 'light'},
-      {name: 'Палпатин', side: 'dark'},
-      {name: 'Обиван Кеноби', side: 'light'}
-    ]
-  }
-
-  render() {
-    return (
-      <ul>
-        {this.state.starWarsChars.map((char, index) => {
-          return (
-            <li key={char.name + index}>
-              <strong>{char.name}</strong> - {char.side}
-            </li>
-          )
-        })}
-
-      </ul>
-    )
-  }
+const App = ({list, side}) => {
+  const filteredList = list.filter(char => char.side === side)
+  return (
+    <ul>
+      {filteredList.map((char, index) => {
+        return (
+          <li key={char.name + index}>
+            <strong>{char.name}</strong> - {char.side}
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App list={starWarsChars} side="light"/>, document.getElementById('root'))
